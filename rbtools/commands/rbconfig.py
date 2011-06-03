1d89bb8797ddbe373e5f299a252241d9390b6930
Alexander Solovets <asolovets@gmail.com>
2011-06-03 20:12:22 +1100

    Make config loading code less repeatable.

--- rbtools/commands/rbconfig.py
+++ rbtools/commands/rbconfig.py
@@ -6,25 +6,25 @@ from rbtools.api.settings import Settings
 def main():
     p = optparse.OptionParser(prog='rb config')
 
-    p.add_option('-u', '--user', action='store', dest='user', help='user name')
-    p.add_option('-U', '--url', action='store', dest='server_url',
-                 help='path to ReviewBoard server')
+    p.add_option('-u', '--user', dest='user', help='user name')
+    p.add_option('-U', '--url', dest='server_url',
+                 help='URL of ReviewBoard server')
     p.add_option('-c', '--cookie-file', action='store', dest='cookie')
 
     opts, args = p.parse_args()
-    settings = Settings()
-    has_options = False
-    if opts.user:
-        settings.add_setting('user_name', opts.user)
-        has_options = True
-    if opts.server_url:
-        settings.add_setting('server_url', opts.server_url)
-        has_options = True
-    if opts.cookie:
-        settings.add_setting('cookie', opts.cookie)
-        has_options = True
+    values = {
+            'user_name' : opts.user,
+            'server_url' : opts.server_url,
+            'cookie' : opts.cookie
+            }
+    # filter out those values are None
+    valid = [ (name, values[name]) for name in values if values[name] ]
 
-    if has_options:
+    settings = Settings()
+    if len(valid):
+        for opt in valid:
+            name, value = opt
+            settings.add_setting(name, value)
         settings.save()
     else:
         p.print_usage()
