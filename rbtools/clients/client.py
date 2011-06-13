c4bb5ab3d956acbd356e2caad4b28c09798d0710
Alexander Solovets <asolovets@gmail.com>
2011-06-13 14:26:25 +1100

    Pull out clients and repo classes into separate files under rbtools/clients.

---
+++ rbtools/clients/client.py
@@ -0,0 +1,103 @@
+class SCMClient(object):
+    """
+    A base representation of an SCM tool for fetching repository information
+    and generating diffs.
+    """
+    def get_repository_info(self):
+        return None
+
+    def check_options(self):
+        pass
+
+    def scan_for_server(self, repository_info):
+        """
+        Scans the current directory on up to find a .reviewboard file
+        containing the server path.
+        """
+        server_url = None
+
+        if user_config:
+            server_url = self._get_server_from_config(user_config,
+                                                      repository_info)
+
+        if not server_url:
+            for config in configs:
+                server_url = self._get_server_from_config(config,
+                                                          repository_info)
+
+                if server_url:
+                    break
+
+        return server_url
+
+    def diff(self, args):
+        """
+        Returns the generated diff and optional parent diff for this
+        repository.
+
+        The returned tuple is (diff_string, parent_diff_string)
+        """
+        return (None, None)
+
+    def diff_between_revisions(self, revision_range, args, repository_info):
+        """
+        Returns the generated diff between revisions in the repository.
+        """
+        return (None, None)
+
+    def _get_server_from_config(self, config, repository_info):
+        if 'REVIEWBOARD_URL' in config:
+            return config['REVIEWBOARD_URL']
+        elif 'TREES' in config:
+            trees = config['TREES']
+            if not isinstance(trees, dict):
+                die("Warning: 'TREES' in config file is not a dict!")
+
+            # If repository_info is a list, check if any one entry is in trees.
+            path = None
+
+            if isinstance(repository_info.path, list):
+                for path in repository_info.path:
+                    if path in trees:
+                        break
+                else:
+                    path = None
+            elif repository_info.path in trees:
+                path = repository_info.path
+
+            if path and 'REVIEWBOARD_URL' in trees[path]:
+                return trees[path]['REVIEWBOARD_URL']
+
+        return None
+
+
+class RepositoryInfo:
+    """
+    A representation of a source code repository.
+    """
+    def __init__(self, path=None, base_path=None, supports_changesets=False,
+                 supports_parent_diffs=False):
+        self.path = path
+        self.base_path = base_path
+        self.supports_changesets = supports_changesets
+        self.supports_parent_diffs = supports_parent_diffs
+        debug("repository info: %s" % self)
+
+    def __str__(self):
+        return "Path: %s, Base path: %s, Supports changesets: %s" % \
+            (self.path, self.base_path, self.supports_changesets)
+
+    def set_base_path(self, base_path):
+        if not base_path.startswith('/'):
+            base_path = '/' + base_path
+        debug("changing repository info base_path from %s to %s" % \
+              (self.base_path, base_path))
+        self.base_path = base_path
+
+    def find_server_repository_info(self, server):
+        """
+        Try to find the repository from the list of repositories on the server.
+        For Subversion, this could be a repository with a different URL. For
+        all other clients, this is a noop.
+        """
+        return self
