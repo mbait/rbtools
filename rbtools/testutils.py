ca4c85d93cbec806d397ca02843ad1c89d51d024
Alexander Solovets <asolovets@gmail.com>
2011-07-03 10:57:00 +1100

    Helper classes for testing.

--- rbtools/testutils.py
+++ rbtools/testutils.py
@@ -1,4 +1,45 @@
-# We intentionally use such values that hardly ever can be met in real config.
-TEST_SERVER = '54E1889C-9EE2-11E0-90AF-98B94724019B'
-TEST_USER = '601EFDD4-9EE2-11E0-B2D6-9FB94724019B'
-TEST_COOKIE_FILE = '10689F14-9EFD-11E0-901A-19DA4724019B'
+import os
+import unittest
+import uuid
+
+from tempfile import mkdtemp
+
+
+SETTINGS_ATTRS = [
+    'reviewboard_url',
+    'user',
+    'cookie_file'
+]
+
+
+class RBTestBase(unittest.TestCase):
+
+    def chdir_tmp(self, dir=None):
+        return os.chdir(mkdtemp(dir=dir))
+
+    def gen_uuid(self):
+        return str(uuid.uuid4())
+
+    def get_user_home(self):
+        return os.environ['HOME']
+
+    def set_user_home(self, path):
+        os.environ['HOME'] = path
+
+    def set_user_home_tmp(self):
+        self.set_user_home(mkdtemp())
+
+
+class SettingsTestBase(RBTestBase):
+
+    def check_fake_settings(self, settings, samples):
+        for name in samples:
+            self.assertEqual(getattr(settings, name), samples[name])
+
+    def set_fake_settings(self, settings, attrs=SETTINGS_ATTRS):
+        fake_data = {}
+        for name in attrs:
+            fake_data[name] = self.gen_uuid()
+            setattr(settings, name, fake_data[name])
+
+        return fake_data
