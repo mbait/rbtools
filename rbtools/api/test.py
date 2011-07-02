cb6b60759c9eca9899dbcfc05cf4920236b2bd9b
Alexander Solovets <asolovets@gmail.com>
2011-07-03 10:59:58 +1100

    Test "Settings" behaviour at loading both global and local files, change testing workflow.

--- rbtools/api/test.py
+++ rbtools/api/test.py
@@ -1,49 +1,30 @@
 import os
 import sys
-import unittest
 
-from tempfile import mkdtemp
-
-from rbtools.api.settings import Settings, CONFIG_NAME
+from rbtools.api.settings import Settings
 from rbtools.api.utilities import  RBUtilities
-from rbtools.testutils import TEST_COOKIE_FILE, TEST_SERVER, TEST_USER
+from rbtools.testutils import RBTestBase, SettingsTestBase
 
-class SettingsTest(unittest.TestCase):
 
-    def set_fake_settings(self, settings):
-        settings.reviewboard_url =  TEST_SERVER
-        settings.user = TEST_USER
-        settings.cookie_file = TEST_COOKIE_FILE
+class SettingsTest(SettingsTestBase):
 
-    def check_fake_settings(self, settings):
-        self.assertEqual(settings.reviewboard_url, TEST_SERVER)
-        self.assertEqual(settings.user, TEST_USER)
-        self.assertEqual(settings.cookie_file, TEST_COOKIE_FILE)
-
-    def test_get_settings(self):
-        os.chdir(mkdtemp())
-        f = open(CONFIG_NAME, 'w')
-        f.write(FAKE_CONFIG)
-        f.close()
-
-        settings = Settings()
-        settings.load()
-        self.check_fake_settings(settings)
+    def setUp(self):
+        self.set_user_home_tmp()
 
     def test_set_settings(self):
         settings = Settings()
-        self.set_fake_settings(settings)
-        self.check_fake_settings(settings)
+        data = self.set_fake_settings(settings)
+        self.check_fake_settings(settings, data)
 
     def test_save_and_load_settings(self):
-        os.chdir(mkdtemp())
+        self.chdir_tmp()
         settings = Settings()
-        self.set_fake_settings(settings)
-        settings.save(CONFIG_NAME)
+        data = self.set_fake_settings(settings)
+        settings.save_local()
 
         settings = Settings()
         settings.load()
-        self.check_fake_settings(settings)
+        self.check_fake_settings(settings, data)
 
     def test_set_attr(self):
         """Settings class is know to overwrite __getattr__ and __setattr__"""
@@ -58,7 +39,34 @@ class SettingsTest(unittest.TestCase):
         with self.assertRaises(AttributeError):
             settings.another_fake_attribute
 
-class UtilitiesTest(unittest.TestCase):
+    def test_use_global(self):
+        self.chdir_tmp(dir=self.get_user_home())
+        settings = Settings()
+        data = self.set_fake_settings(settings)
+        settings.save_global()
+
+        settings = Settings()
+        settings.load()
+        self.check_fake_settings(settings, data)
+
+    def test_use_local_and_global(self):
+        self.set_user_home_tmp()
+        self.chdir_tmp(dir=self.get_user_home())
+        settings = Settings()
+        global_data = self.set_fake_settings(settings, attrs=['cookie_file'])
+        settings.save_global()
+
+        settings = Settings()
+        local_data = self.set_fake_settings(settings, attrs=['user'])
+        settings.save_local()
+
+        settings = Settings()
+        settings.load()
+        global_data.update(local_data)
+        self.check_fake_settings(settings, global_data)
+
+
+class UtilitiesTest(RBTestBase):
 
     def test_check_install(self):
         util = RBUtilities()
@@ -85,13 +93,3 @@ class UtilitiesTest(unittest.TestCase):
         with self.assertRaises(SystemExit) as rc:
             util.die()
         self.assertEqual(rc.exception.code, 1)
-
-
-FAKE_CONFIG = """\
-[main]
-
-ReVIeWBoard_UrL = %s
-
-USer = %s
-cookie_file =%s
-""" % (TEST_SERVER, TEST_USER, TEST_COOKIE_FILE)
