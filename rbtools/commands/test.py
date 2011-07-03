62808312fa6ae3fa8b643d02c66b2a23ba17112a
Alexander Solovets <asolovets@gmail.com>
2011-07-03 15:54:43 +1100

    Rewrite rb-config tests, test --global option.

--- rbtools/commands/test.py
+++ rbtools/commands/test.py
@@ -1,39 +1,95 @@
-import os
 import sys
-import unittest
-
-from tempfile import mkdtemp
 
 from rbtools.api.settings import Settings
-from rbtools.commands.rbconfig import main as rbconfig_main
-from rbtools.testutils import TEST_COOKIE_FILE, TEST_SERVER, TEST_USER
+from rbtools.commands.rbconfig import main as rbconfig
+from rbtools.testutils import RBTestBase
 
 
-class RBConfigTest(unittest.TestCase):
+OPT_MAPPINGS = {
+    '-U': 'reviewboard_url',
+    '-u': 'user',
+    '-c': 'cookie_file',
+    '--url': 'reviewboard_url',
+    '--user': 'user',
+    '--cookie-file': 'cookie_file'
+}
 
-    def _set_and_check_options(self, opts):
-        sys.argv = opts
-        os.chdir(mkdtemp())
-        rbconfig_main()
 
-        settings = Settings()
-        settings.load()
-        self.assertEqual(settings.reviewboard_url, TEST_SERVER)
-        self.assertEqual(settings.user, TEST_USER)
-        self.assertEqual(settings.cookie_file, TEST_COOKIE_FILE)
+class RBConfigTest(RBTestBase):
+
+    def setUp(self):
+        self.set_user_home_tmp()
+
+    def check_loaded_values(self, settings, samples):
+        for name in samples:
+            self.assertEqual(samples[name], getattr(settings, name))
+
+    def set_fake_values(self, *opts):
+        args = ['']
+        data = {}
+        for name, value in [(name, self.gen_uuid()) for name in opts]:
+            args.append(name)
+            args.append(value)
+            data[OPT_MAPPINGS[name]] = value
+
+        self.reset_cl_args(values=args)
+        return data
 
     def test_run_blank(self):
-        # Without any argument program should still exit with zero error code.
-        sys.argv = []
+        # Without any arguments program should still exit with zero error code.
+        self.reset_cl_args()
         try:
-            rbconfig_main()
+            rbconfig()
         except Exception:
             self.fail()
 
-    def test_set_options(self):
-        self._set_and_check_options(['', '-u', TEST_USER, '-U', TEST_SERVER,
-                                     '-c', TEST_COOKIE_FILE])
+    def test_set_options_short(self):
+        self.chdir_tmp()
+        data = self.set_fake_values('-u', '-U', '-c')
+        rbconfig()
+
+        settings = Settings()
+        settings.load()
+        self.check_loaded_values(settings, data)
 
     def test_set_options_long(self):
-        self._set_and_check_options(['', '--user', TEST_USER, '--url', TEST_SERVER,
-                                     '--cookie-file', TEST_COOKIE_FILE])
+        self.chdir_tmp()
+        data = self.set_fake_values('--user', '--url', '--cookie-file')
+        rbconfig()
+
+        settings = Settings()
+        settings.load()
+        self.check_loaded_values(settings, data)
+
+    def test_set_options_mixed(self):
+        self.chdir_tmp()
+        data = self.set_fake_values('-c', '--user')
+        rbconfig()
+
+        settings = Settings()
+        settings.load()
+        self.check_loaded_values(settings, data)
+
+    def test_set_options_global(self):
+        self.chdir_tmp()
+        data = self.set_fake_values('-u', '-U', '-c')
+        sys.argv.append('--global')
+        rbconfig()
+
+        settings = Settings()
+        settings.load()
+        self.check_loaded_values(settings, data)
+
+    def test_set_options_local_and_global(self):
+        self.chdir_tmp()
+        data = self.set_fake_values('-u', '-U')
+        sys.argv.append('--global')
+        rbconfig()
+
+        self.chdir_tmp(dir=self.get_user_home())
+        data.update(self.set_fake_values('--cookie-file'))
+        rbconfig()
+
+        settings = Settings()
+        settings.load()
+        self.check_loaded_values(settings, data)
