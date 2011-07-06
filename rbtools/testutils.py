0dd507c2ec36fc243e0e02c87ffada108ebdc2f4
Alexander Solovets <asolovets@gmail.com>
2011-07-06 23:32:13 +1100

    Add in_path() method of testutils wich checks binary existence in environment variable PATH.

--- rbtools/testutils.py
+++ rbtools/testutils.py
@@ -22,6 +22,26 @@ class RBTestBase(unittest.TestCase):
     def get_user_home(self):
         return os.environ['HOME']
 
+    def in_path(self, binary):
+        """Checks whether an executable is in the user's search path.
+
+        This expects a name without any system-specific executable extension.
+        It will append the proper extension as necessary. For example,
+        use "myapp" and not "myapp.exe".
+
+        This will return True if the app is in the path, or False otherwise.
+
+        Taken from djblets.util.filesystem to avoid an extra dependency
+            """
+        if sys.platform == 'win32' and not binary.endswith('.exe'):
+            binary += ".exe"
+
+        for dir in os.environ['PATH'].split(os.pathsep):
+            if os.path.exists(os.path.join(dir, binary)):
+                return True
+
+        return False
+
     def reset_cl_args(self, values=[]):
         sys.argv = values
 
