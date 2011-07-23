76fc4124b45c63c08a3616f50e8b5447fe068439
Alexander Solovets <asolovets@gmail.com>
2011-07-23 20:27:59 +1100

    Rewrite rb-patch using implied new API.

--- rbtools/commands/rbpatch.py
+++ rbtools/commands/rbpatch.py
@@ -1,87 +1,98 @@
-import os
-import re
-import string
 import sys
-from urllib2 import HTTPError
+
+from optparse import OptionParser
 
 from rbtools.api.settings import Settings
-from rbtools.api.resource import Resource, \
-                                RootResource, \
-                                ReviewRequest, \
-                                ResourceList, \
-                                DiffResource
-from rbtools.api.serverinterface import ServerInterface
-from rbtools.clients.getclient import get_client
-
-
-COMMIT_OPTION = '-c'
 
 
 def main():
-    valid = False
+    parser = OptionParser(prog='rb patch',
+                          usage='%prog [options] <request-id>')
+
+    parser.add_option('-r', '--revision', help='diff revision number')
+    parser.add_option('-s', '--summary', action='store_true',
+                      help='use request summary as commit message')
+    parser.add_option('-d', '--description', action='store_true',
+                      help='use request description as commit message')
+
+    opt, args = parser.parse_args()
+
+    if not args:
+        parser.print_help()
+        sys.exit(1)
+
+    if len(args) > 1:
+        parser.error('unexpected arguments - %s' % args[1:])
+
     settings = Settings()
     settings.load()
+    print '%s/api/review-requests/%s/diffs/%s/' % (settings.reviewboard_url,
+                                                   args[0], opt.revision)
 
-    if len(sys.argv) > 1:
-        valid = True
-        cookie = settings.get_cookie_file()
-        server_url = settings.get_server_url()
-        diff_file_name = 'patch_diff'
-        arg_index = 1        
-        commit = False
+    #valid = False
+    #settings = Settings()
+    #settings.load()
 
-        if sys.argv[arg_index] == COMMIT_OPTION:
-            if len(sys.argv) > 2:
-                commit = True
-                arg_index = arg_index + 1
-            else:
-                valid = False
+    #if len(sys.argv) > 1:
+    #    valid = True
+    #    cookie = settings.get_cookie_file()
+    #    server_url = settings.get_server_url()
+    #    diff_file_name = 'patch_diff'
+    #    arg_index = 1        
+    #    commit = False
 
-        review_request_id = sys.argv[arg_index]
-        arg_index = arg_index + 1
-        server = ServerInterface(server_url, cookie)
-        root = RootResource(server, server_url + 'api/')
-        review_requests = root.get('review_requests')
-        request = ReviewRequest(review_requests.get(review_request_id))
-        diffs = request.get_or_create('diffs')
-        revision_id = len(diffs)
+    #    if sys.argv[arg_index] == COMMIT_OPTION:
+    #        if len(sys.argv) > 2:
+    #            commit = True
+    #            arg_index = arg_index + 1
+    #        else:
+    #            valid = False
 
-        if len(sys.argv) > arg_index:
-            if sys.argv[arg_index].isdigit():
-                revision_id = sys.argv[arg_index]
-            
-        try:
-            diff = DiffResource(diffs.get(revision_id))
-        except HTTPError, e:
-            if e.code == 404:
-                print 'The specified diff revision ' \
-                      'does not exist.'
-                exit()
-            else:
-                raise e
+    #    review_request_id = sys.argv[arg_index]
+    #    arg_index = arg_index + 1
+    #    server = ServerInterface(server_url, cookie)
+    #    root = RootResource(server, server_url + 'api/')
+    #    review_requests = root.get('review_requests')
+    #    request = ReviewRequest(review_requests.get(review_request_id))
+    #    diffs = request.get_or_create('diffs')
+    #    revision_id = len(diffs)
 
-        server_diff = diff.get_diff()
-        diff_file = open(diff_file_name, 'w')
+    #    if len(sys.argv) > arg_index:
+    #        if sys.argv[arg_index].isdigit():
+    #            revision_id = sys.argv[arg_index]
+    #        
+    #    try:
+    #        diff = DiffResource(diffs.get(revision_id))
+    #    except HTTPError, e:
+    #        if e.code == 404:
+    #            print 'The specified diff revision ' \
+    #                  'does not exist.'
+    #            exit()
+    #        else:
+    #            raise e
 
-        if diff_file < 0:
-            print 'could not open "' + diff_file_name + '" for writing.'
-            exit()
+    #    server_diff = diff.get_diff()
+    #    diff_file = open(diff_file_name, 'w')
 
-        diff_file.write(server_diff)
-        diff_file.close()
-        client = get_client(server_url)
-        client.apply_patch(diff_file_name, commit)
+    #    if diff_file < 0:
+    #        print 'could not open "' + diff_file_name + '" for writing.'
+    #        exit()
 
-    if not valid:
-        print 'usage: rb patch [-c] ' \
-              '<review_request_id> [revision_id]'
-        print ''
-        print '[-c] is used to commit the patch, in addition to applying it.'
-        print '[file_name] can be used to set the name of the file to save ' \
-              'the patch to.'
-        print '[revision_id] may be specified to indicate which revision ' \
-              'of the patch in the review request to use.  If unspecified ' \
-              'the most recent revision is used.'
+    #    diff_file.write(server_diff)
+    #    diff_file.close()
+    #    client = get_client(server_url)
+    #    client.apply_patch(diff_file_name, commit)
+
+    #if not valid:
+    #    print 'usage: rb patch [-c] ' \
+    #          '<review_request_id> [revision_id]'
+    #    print ''
+    #    print '[-c] is used to commit the patch, in addition to applying it.'
+    #    print '[file_name] can be used to set the name of the file to save ' \
+    #          'the patch to.'
+    #    print '[revision_id] may be specified to indicate which revision ' \
+    #          'of the patch in the review request to use.  If unspecified ' \
+    #          'the most recent revision is used.'
 
 
 if __name__ == '__main__':
