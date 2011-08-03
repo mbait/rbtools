d01490d4ec266b8a5e9178e5624bf1407a04a1d6
Alexander Solovets <asolovets@gmail.com>
2011-08-03 19:32:18 +1100

    Revert "Rewrite "Settings" class, use pythonic power to construct config accessors."
    This reverts commit cc99f912d8b5075c3a4a6f1301650fc068269c09.
    
    Conflicts:
    
    	rbtools/api/settings.py

--- rbtools/api/settings.py
+++ rbtools/api/settings.py
@@ -1,82 +1,247 @@
 import os
+import sys
 
-from ConfigParser import ConfigParser
+from rbtools.api.utilities import RBUtilities
 
-
-CONFIG_NAME = '.reviewboardrc'
-MAIN_SECTION_NAME = 'main'
-SEPARATOR = '_'
-
-OPTIONS = [
-    (MAIN_SECTION_NAME, 'reviewboard_url'),
-    (MAIN_SECTION_NAME, 'user'),
-    (MAIN_SECTION_NAME, 'cookie_file'),
-    (MAIN_SECTION_NAME, 'api_uri'),
-]
-ATTRIBUTES = [SEPARATOR.join(opt) for opt in OPTIONS]
+BLANK = 0
+USER_ADDED = '#user added settings:'
+COMMENT = '#'
+CONFIG_NAME = '/.reviewboardrc'
 
 
 class Settings(object):
-    """Provides access to user config"""
+    """Handles rbtools settings
 
-    def __init__(self):
-        self.configs = [os.path.join(p, CONFIG_NAME)
-                        for p in [os.path.expanduser('~'), os.getcwd()]]
-        self.options = {}
+    responsible for loading settings, as well as adding new settings.
+    Order of settings and blank lines are kept when loading and saving.
+    Comments (denoted by #) are allowed on blank lines, and after settings
+    Settings are stored <name> <tab> <value>. Names cannot contain spaces,
+    tabs or #. values (which can be strings, ints, floats, or bools) can
+    contain anything except for #. Values will be converted to the correct
+    type if that type is accepted.
+    """
+    config_default = os.getcwd() + CONFIG_NAME
+    config_global = os.path.expanduser('~') + CONFIG_NAME
+    if not os.path.isfile(config_default) and os.path.isfile(config_global):
+        config_default = config_global
 
-    def __getattr__(self, name):
-        full_name = self._full_name(name)
+    util = None
+    config_file = None
+    settings = None
+    settings_comment = None
+    ordered = None
+    none_added = None
 
-        if full_name in ATTRIBUTES:
-            return self.options[full_name]
-        else:
-            raise AttributeError("'%s' is neither attribute nor option name"
-                                 % name)
-
-    def __setattr__(self, name, value):
-        full_name = self._full_name(name)
-
-        if full_name in ATTRIBUTES:
-            self.options[full_name] = value
-        else:
-            super(Settings, self).__setattr__(name, value)
-
-    def _full_name(self, name):
-        """Check if the property <name> is a valid option
-        and prepend MAIN_SECTION_NAME if not."""
-        if not name in OPTIONS:
-            return SEPARATOR.join([MAIN_SECTION_NAME, name])
-        else:
-            return name
+    def __init__(self, util=RBUtilities(), config_file=config_default):
+        super(Settings, self).__init__()
+        self.util = util
+        self.config_file = config_file
+        self.settings = {}
+        self.settings_comment = {}
+        self.ordered = []
+        self.none_added = True
 
     def load(self):
-        """Load options from all possible locations."""
-        # If particular config file was supplied use it
-        # else load default files.
-        cfg = ConfigParser()
-        cfg.read(self.configs)
+        """loads the settings file"""
+        try:
+            # TODO: apply ConfigParser
+            file = open(self.config_file, 'r')
 
-        for sec, name in OPTIONS:
-            if cfg.has_section(sec) and cfg.has_option(sec, name):
-                self.options[SEPARATOR.join([sec, name])] = cfg.get(sec, name)
+            if file < 0:
+                self.util.raise_error('UnknownFileError', \
+                    'Could not open file: ' + self.config_file)
 
-    def save(self, file_name):
-        """Writes config to file."""
-        cfg = ConfigParser()
+            self.settings = {}
+            self.settings_comment = {}
+            self.ordered = []
 
-        for opt in OPTIONS:
-            full_name = SEPARATOR.join(opt)
-            if full_name in self.options:
-                sec, name = opt
-                if not cfg.has_section(sec):
-                    cfg.add_section(sec)
+            line = file.readline()
+            while line:
+                line = line.strip()
 
-                cfg.set(sec, name, self.options[full_name])
+                #filter out blank lines
+                if len(line):
 
-        cfg.write(open(file_name, 'w'))
+                    #handle comments
+                    tuple = line.split(COMMENT)
 
-    def save_local(self):
-        self.save(os.path.join(os.getcwd(), CONFIG_NAME))
+                    if len(tuple[0]) == 0:  # just comment
+                        self.ordered.append(COMMENT + tuple[1])
+                    else:
+                        comment = None
+                        if len(tuple) > 1:  # inline comment
+                            comment = ''
 
-    def save_global(self):
-        self.save(os.path.join(os.path.expanduser('~'), CONFIG_NAME))
+                            for i in range(1, len(tuple)):
+                                comment += COMMENT + tuple[i]
+
+                        tuple = tuple[0].split()
+
+                        #validate setting
+                        if len(tuple) < 2:
+                            self.util.raise_warning('Invalid_Type', \
+                                'could not read setting: ' + line)
+                        else:
+                            setting = tuple[1]
+
+                            if len(tuple) > 2:  # setting had spaces
+
+                                for i in range(2, len(tuple)):
+                                    setting += ' ' + tuple[i]
+
+                            #convert the value to the correct type
+                            dec = setting.split('.')
+                            if setting.isdigit():
+                                setting = int(setting)
+                            elif len(dec) == 2 and dec[0].isdigit() \
+                                    and dec[1].isdigit():
+                                setting = float(setting)
+                            elif setting is 'True':
+                                setting = True
+                            elif setting is 'False':
+                                setting = False
+
+                            #store comment
+                            name = tuple[0]
+                            self.ordered.append(name)
+                            self.settings[name] = setting
+
+                            if comment:
+                                self.settings_comment[name] = comment
+                else:
+                    self.ordered.append(BLANK)
+
+                line = file.readline()
+
+            file.close()
+        except IOError as (errno, strerror):
+            self.util.die('Failed to load config from "%s": %s' %
+                          (self.config_file, strerror))
+
+    def save(self):
+        """saves the current settings"""
+        keys = self.settings.keys()
+
+        file = open(self.config_file, 'w')
+
+        if file < 0:
+            self.util.raise_error('UnknownFileError', \
+                    'Could not open file: ' + config_file)
+
+        for key in keys:
+            if key is BLANK:
+                file.write('\n')
+            elif key[0] == COMMENT:
+                file.write(key + '\n')
+            else:
+                file.write(key + '\t' + self.settings[key])
+
+                if key in self.settings_comment:
+                    file.write('\t' + self.settings_comment[key] + '\n')
+                else:
+                    file.write('\n')
+
+        file.close()
+
+    def clear(self):
+        """ Clears the settings.
+
+        After called, the config_file will be empty and if save() were to be
+        called no settings or comments would be added into the config_file.
+        """
+        self.settings = {}
+        self.settings_comment = {}
+        self.ordered = []
+        file = open(self.config_file, 'w')
+        file.close()
+
+    def change_setting(self, name, value, comment=None):
+        """changes the value of a setting"""
+
+        if not name in self.settings:
+            self.util.raise_warning('UnrecognizedSetting', 'Setting "' \
+                + name + '" does not exist. Adding it in and setting it to "' \
+                + value + '"')
+            self.add_setting(name, value, comment)
+        else:
+            self.settings[name] = value
+
+            if comment:
+                self.settings_comment[name] = COMMENT + comment
+
+            self.save()
+
+    def add_setting(self, name, value, comment=None):
+        """adds a setting to the settings"""
+        if name in self.settings:
+            self.util.raise_warning('DuplicateSetting', 'Setting "' + name + \
+                '" already exists. Changing its value to "' + value + '"')
+            self.change_setting(name, value, comment)
+        else:
+            self.settings[name] = value
+
+            file = open(self.config_file, 'a')
+
+            if file < 0:
+                self.util.raise_error('UnknownFileError', \
+                    'Could not open file: ' + config_file)
+
+            if self.none_added:  # first user added comment (this session)
+                file.write('\n' + USER_ADDED + '\n')
+            self.none_added = False
+
+            file.write(name + '\t' + value)
+
+            if comment:
+                comment = COMMENT + comment
+                self.settings_comment[name] = comment
+                file.write('\t' + comment + '\n')
+            else:
+                file.write('\n')
+
+            file.close()
+
+    def remove_setting(self, name):
+        """removes a setting"""
+        if name not in self.settings:
+            self.util.raise_warning('UnknownSetting', 'Setting "' + name + \
+                '" does not exist')
+
+        del self.settings[name]
+        self.save()
+
+    def get_setting(self, name):
+        """returns a setting"""
+        value = None
+
+        if name not in self.settings:
+            self.util.raise_warning('UnknownSetting', 'Setting "' + name + \
+                '" does not exist')
+        else:
+            value = self.settings[name]
+
+        return value
+
+    def set_server_url(self, value):
+        self.change_setting('server_url', value)
+
+    def set_api_uri(self, value):
+        self.change_setting('api_extension', value)
+
+    def set_cookie_file(self, value):
+        self.change_setting('cookie', value)
+
+    def set_user_name(self, value):
+        self.change_setting('user_name', value)
+
+    def get_server_url(self):
+        return self.get_setting('server_url')
+
+    def get_api_uri(self):
+        return self.get_setting('api_extension')
+
+    def get_cookie_file(self):
+        return self.get_setting('cookie')
+
+    def get_user_name(self):
+        return self.get_setting('user_name')
