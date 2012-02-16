71366822eb8fc5bb579c6ef116640d947cac5da2
Alexander Solovets <asolovets@gmail.com>
2012-02-16 13:53:05 +1100

    Separate two input operations.

--- rbtools/api/utils.py
+++ rbtools/api/utils.py
@@ -4,11 +4,13 @@ import getpass
 class AuthReader(object):
     """Generic interface for requesting authentication data."""
     def get_auth_data(self, realm, uri):
-        raise NotImplementedError
+        raise NotImplementedError()
 
 
 class TerminalAuthReader(AuthReader):
     """Requests authentication data via terminal prompts."""
     def get_auth_data(self, realm, uri):
         print "Enter username and password for %s at %s" % (realm, uri)
-        return raw_input('User: '), getpass.getpass('Password: ')
+        user = raw_input('User: ')
+        password  = getpass.getpass('Password: ')
+        return user, password
