0bf125f57f5c9f4cb0cb3b66a8762eedae485b6e
Alexander Solovets <asolovets@gmail.com>
2012-04-04 17:37:58 +1100

    Added test suit for HttpRequest.

--- rbtools/api/tests.py
+++ rbtools/api/tests.py
@@ -1,4 +1,5 @@
 from rbtools.api.builder import ResourceBuilder
+from rbtools.api.request import HttpRequest
 from rbtools.utils.testbase import RBTestBase
 
 
@@ -6,9 +7,16 @@ class StubTransport(object):
     pass
 
 
-class ResourceGeneratorTests(RBTestBase):
+class HttpRequestTest(RBTestBase):
+    def setUp(self):
+        self.request = HttpRequest()
+
+    def test_default_values(self):
+        self.assertEquals(self.request.get_body(), '')
+        self.assertEquals(self.request.get_method(), 'GET')
+        self.assertEquals(self.request.get_mime_type(), 'application/json')
+
+
+class ResourceBuilderTests(RBTestBase):
     def setUp(self):
         self.builder = ResourceBuilder(StubTransport())
-
-    def testGetRoot(self):
-        pass
