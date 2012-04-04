dbaeb6dc03ba88ed36ec7c6cd463bf982a7ca579
Alexander Solovets <asolovets@gmail.com>
2012-04-04 18:07:49 +1100

    Naming style issue: test class names should be plural. Introduced 'url' parameter of the MockTransport constructor. Added two tests for ResourceBuilder - check if it creates right classes.

--- rbtools/api/tests.py
+++ rbtools/api/tests.py
@@ -1,22 +1,35 @@
 from rbtools.api.builder import ResourceBuilder
 from rbtools.api.request import HttpRequest
+from rbtools.api.resource import Resource
 from rbtools.utils.testbase import RBTestBase
 
 
-class StubTransport(object):
+class MockTransport(object):
+    def __init__(self, url):
+        self._url = url
+
+
+class StubResource(Resource):
     pass
 
 
-class HttpRequestTest(RBTestBase):
+class HttpRequestTests(RBTestBase):
     def setUp(self):
         self.request = HttpRequest()
 
     def test_default_values(self):
         self.assertEquals(self.request.get_body(), '')
         self.assertEquals(self.request.get_method(), 'GET')
-        self.assertEquals(self.request.get_mime_type(), 'application/json')
 
 
 class ResourceBuilderTests(RBTestBase):
     def setUp(self):
-        self.builder = ResourceBuilder(StubTransport())
+        self.builder = ResourceBuilder(MockTransport('local'))
+
+    def test_build_generic_resource(self):
+        resource = self.builder.build(Resource, {'stat': 'ok'})
+        self.assertIsInstance(resource, Resource)
+
+    def test_build_custom_resource(self):
+        resource = self.builder.build(StubResource, {'stat': 'ok'})
+        self.assertIsInstance(resource, StubResource)
