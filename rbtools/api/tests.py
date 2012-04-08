ee4bd824dd44d958c588e152b553f43bcd59ac17
Alexander Solovets <asolovets@gmail.com>
2012-04-08 11:40:52 +1100

    Added tests for MockTransport.

--- rbtools/api/tests.py
+++ rbtools/api/tests.py
@@ -1,25 +1,91 @@
+import simplejson
+import unittest
+
 from rbtools.api.builder import ResourceBuilder
 from rbtools.api.request import HttpRequest
 from rbtools.api.resource import Resource
-from rbtools.utils.testbase import RBTestBase
-
-
-class MockTransport(object):
-    def __init__(self):
-        self._resources = {}
-
-    def add_resource(self, uri, resource):
-        self._resources[uri] = resource
-
-    def send_request(self, request, callback):
-        pass
 
 
 class StubResource(Resource):
     pass
 
 
-class HttpRequestTests(RBTestBase):
+class MockTransport(object):
+    def __init__(self):
+        self._resources = {}
+
+    def _create_payload(self, path):
+        return {}
+
+    def get_payload(self, path):
+        payload = {'stat': 'ok'}
+        resource = payload
+        names = [r for r in path.split('/') if r]
+
+        if names:
+            res_name = names[-2][:-1]
+            resource[res_name] = {'id': names[-1]}
+            resource = resource[res_name]
+            # Set previously saved properties.
+            resource.update(self._resources[path])
+        else:
+            # Construct uri_templates
+            resource['uri_templates'] = {'root': '/'}
+
+        # Fill links.
+        links = {
+            ResourceBuilder.SELF_TOK: {
+                'method': 'GET',
+                'href': path
+            }
+        }
+
+        resource[ResourceBuilder.LINKS_TOK] = links
+
+        return payload
+
+    def set_resource(self, path, resource):
+        self._resources[path] = resource
+
+    def send_request(self, request, callback):
+        pl = self._create_payload(request.get_url())
+        callback(simplejson.dumps(pl))
+
+
+class MockTransportTests(unittest.TestCase):
+    def setUp(self):
+        self._transport = MockTransport()
+
+    def assertInAndEquals(self, key, value, dct):
+        self.assertIn(key, dct)
+        self.assertEquals(dct[key], value)
+
+    def test_create_root_resource(self):
+        payload = self._transport.get_payload('/')
+
+        self.assertInAndEquals('stat', 'ok', payload)
+
+        self.assertIn(ResourceBuilder.LINKS_TOK, payload)
+        links = payload[ResourceBuilder.LINKS_TOK]
+        self.assertIn(ResourceBuilder.SELF_TOK, links)
+        self.assertInAndEquals('href', '/', links[ResourceBuilder.SELF_TOK])
+        self.assertInAndEquals('method', 'GET',
+                               links[ResourceBuilder.SELF_TOK])
+
+        self.assertIn('uri_templates', payload)
+        self.assertInAndEquals('root', '/', payload['uri_templates'])
+
+    def test_create_non_root_resource(self):
+        self._transport.set_resource('/foos/42/', {'attr': 'name'})
+        payload = self._transport.get_payload('/foos/42/')
+        self.assertInAndEquals('stat', 'ok', payload)
+        self.assertIn('foo', payload)
+        foo = payload['foo']
+        self.assertInAndEquals('id', '42', foo)
+        self.assertInAndEquals('attr', 'name', foo)
+
+
+class HttpRequestTests(unittest.TestCase):
     def test_default_values(self):
         request = HttpRequest('/')
         self.assertEquals(request.get_url(), '/')
@@ -31,27 +97,28 @@ class HttpRequestTests(RBTestBase):
         self.assertEquals(request.get_method(), 'POST')
 
 
-class ResourceBuilderTests(RBTestBase):
+class ResourceBuilderTests(unittest.TestCase):
     def setUp(self):
         transport = MockTransport()
-        transport.add_resource('/foos/1/',  {'prop': 'x'})
-        transport.add_resource('/foos/2/',  {'prop': 'y'})
-        transport.add_resource('/foos/3/',  {'prop': 'z'})
-        transport.add_resource('/bars/1/',  {'field': 'foo'})
-        transport.add_resource('/bars/2/',  {'field': 'bar'})
-        transport.add_resource('/bars/3/',  {'field': 'baz'})
-        transport.add_resource('/foos/1/bazes/1/',  {'attr': '101'})
-        transport.add_resource('/foos/2/bazes/2/',  {'attr': '102'})
-        transport.add_resource('/foos/3/bazes/3/',  {'attr': '103'})
-        transport.add_resource('/foos/3/bazes/3/quxes/1/',  {'value': 'name'})
+        transport.set_resource('/foos/1/',  {'prop': 'x'})
+        transport.set_resource('/foos/2/',  {'prop': 'y'})
+        transport.set_resource('/foos/3/',  {'prop': 'z'})
+        transport.set_resource('/bars/1/',  {'field': 'foo'})
+        transport.set_resource('/bars/2/',  {'field': 'bar'})
+        transport.set_resource('/bars/3/',  {'field': 'baz'})
+        transport.set_resource('/foos/1/bazes/1/',  {'attr': '101'})
+        transport.set_resource('/foos/2/bazes/2/',  {'attr': '102'})
+        transport.set_resource('/foos/3/bazes/3/',  {'attr': '103'})
+        transport.set_resource('/foos/3/bazes/3/quxes/1/',  {'value': 'name'})
 
-        transport.add_resource('/reqs/1/', {'author': 'user1', 'group': 'dev'})
-        transport.add_resource('/reqs/2/', {'author': 'user2', 'group': 'gfx'})
-        transport.add_resource('/reqs/3/', {'author': 'user3', 'group': 'sfx'})
-        transport.add_resource('/reqs/1/revs/', {'file': 'main.cpp'})
-        transport.add_resource('/reqs/2/revs/', {'file': 'layout.svg'})
-        transport.add_resource('/reqs/3/revs/', {'file': 'ambient.wav'})
+        transport.set_resource('/reqs/1/', {'author': 'user1', 'group': 'dev'})
+        transport.set_resource('/reqs/2/', {'author': 'user2', 'group': 'gfx'})
+        transport.set_resource('/reqs/3/', {'author': 'user3', 'group': 'sfx'})
+        transport.set_resource('/reqs/1/revs/', {'file': 'main.cpp'})
+        transport.set_resource('/reqs/2/revs/', {'file': 'layout.svg'})
+        transport.set_resource('/reqs/3/revs/', {'file': 'ambient.wav'})
 
+        self.transport = transport
         self.builder = ResourceBuilder(transport)
 
     def test_build_generic_resource(self):
