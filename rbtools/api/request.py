a47ee9f3bafa4d8d5529465ec2302fc57e6877d2
Alexander Solovets <asolovets@gmail.com>
2012-05-10 22:10:23 +1100

    Test HttpRequest's form data encoding.

--- rbtools/api/request.py
+++ rbtools/api/request.py
@@ -2,13 +2,13 @@ import mimetools
 
 
 class HttpRequest(object):
-    """ High-level HTTP-request object."""
+    """High-level HTTP-request object."""
     def __init__(self, url, method='GET'):
         self._url = url
         self._method = method
         self._headers = {}
-        self.fields = {}
-        self.files = {}
+        self._fields = {}
+        self._files = {}
 
     def get_url(self):
         return self._url
@@ -16,11 +16,26 @@ class HttpRequest(object):
     def get_method(self):
         return self._method
 
+    def set_method(self, method):
+        self._method = method
+
     def has_header(self, name):
         return name in self._headers
 
     def set_header(self, name, value):
         self._headers[name] = value
+
+    def add_field(self, name, value):
+        self._fields[name] = value
+
+    def add_file(self, filename):
+        self._files[filename] = 1
+
+    def del_field(self, name):
+        del self._fields[name]
+
+    def del_file(self, filename):
+        del self._files[filename]
 
     def get_body(self):
         """ Encodes data for use in an HTTP request.
@@ -31,22 +46,22 @@ class HttpRequest(object):
             files  - the files to be encoded.  This should be a dict in a
                      key:dict, filename:value and content:value format
         """
-        if not (self.fields or self.files):
-            return ''
+        if not (self._fields or self._files):
+            return None
 
         NEWLINE = '\r\n'
         BOUNDARY = mimetools.choose_boundary()
         content = ''
 
-        for key in self.fields:
+        for key in self._fields:
             content += '--' + BOUNDARY + NEWLINE
             content += 'Content-Disposition: form-data; name="%s"' % key
             content += NEWLINE + NEWLINE
-            content += self.fields[key] + NEWLINE
+            content += str(self._fields[key]) + NEWLINE
 
-        for key in self.files:
-            filename = self.files[key]['filename']
-            value = self.files[key]['content']
+        for key in self._files:
+            filename = self._files[key]['filename']
+            value = self._files[key]['content']
             content += '--' + BOUNDARY + NEWLINE
             content += 'Content-Disposition: form-data; name="%s"; ' % key
             content += 'filename="%s"' % filename + NEWLINE
--- rbtools/api/tests.py
+++ rbtools/api/tests.py
@@ -1,7 +1,6 @@
-import simplejson
+import re
 import unittest
 
-from rbtools.api.builder import ResourceBuilder
 from rbtools.api.request import HttpRequest
 from rbtools.api.resource import Resource
 
@@ -10,156 +9,47 @@ class StubResource(Resource):
     pass
 
 
-class MockTransport(object):
-    def __init__(self):
-        self._resources = {}
-
-    def _create_payload(self, path):
-        return {}
-
-    def get_payload(self, path):
-        payload = {'stat': 'ok'}
-        resource = payload
-
-        names = [name for name in path.split('/') if name]
-        children = [res for res in self._resources if res.startswith(path)]
-
-        if names:
-            res_name = names[-2][:-1]
-            resource[res_name] = {'id': names[-1]}
-            resource = resource[res_name]
-            # Set previously saved properties.
-            resource.update(self._resources[path])
-        else:
-            # Construct uri_templates
-            resource['uri_templates'] = {'root': '/'}
-
-        # Fill links.
-        links = {
-            ResourceBuilder.SELF_TOK: {
-                'method': 'GET',
-                'href': path
-            }
-        }
-
-        for child in children:
-            names = [r for r in child.split('/') if r]
-            child_name = names[-2]
-            links[child_name] = {'method': 'GET',
-                                 'href': path + child_name + '/'}
-
-        resource[ResourceBuilder.LINKS_TOK] = links
-
-        return payload
-
-    def set_resource(self, path, resource):
-        self._resources[path] = resource
-
-    def send_request(self, request, callback):
-        if request.get_method() == 'GET':
-            pl = self._create_payload(request.get_url())
-            callback(simplejson.dumps(pl))
-        else:
-            pass
-
-
-class MockTransportTests(unittest.TestCase):
-    def setUp(self):
-        self._transport = MockTransport()
-
-    def assertInAndEquals(self, key, value, dct):
-        self.assertIn(key, dct)
-        self.assertEquals(dct[key], value)
-
-    def assertIsRootResource(self, payload):
-        self.assertInAndEquals('stat', 'ok', payload)
-        self.assertIn(ResourceBuilder.LINKS_TOK, payload)
-        links = payload[ResourceBuilder.LINKS_TOK]
-        self.assertIn(ResourceBuilder.SELF_TOK, links)
-        self.assertInAndEquals('href', '/', links[ResourceBuilder.SELF_TOK])
-        self.assertInAndEquals('method', 'GET',
-                               links[ResourceBuilder.SELF_TOK])
-
-        self.assertIn('uri_templates', payload)
-        self.assertInAndEquals('root', '/', payload['uri_templates'])
-
-    def assertIsChildResource(self, payload, name):
-        self.assertInAndEquals('stat', 'ok', payload)
-        self.assertIn(name, payload)
-        self.assertIsInstance(payload[name], dict) 
-
-    def test_create_root_resource(self):
-        self.assertIsRootResource(self._transport.get_payload('/'))
-
-    def test_create_non_root_resource(self):
-        self._transport.set_resource('/foos/42/', {'attr': 'name'})
-        payload = self._transport.get_payload('/foos/42/')
-        self.assertIsChildResource(payload, 'foo')
-
-        foo = payload['foo']
-        self.assertInAndEquals('id', '42', foo)
-        self.assertInAndEquals('attr', 'name', foo)
-
-    def test_create_links_for_child_resources(self):
-        self._transport.set_resource('/foos/42/', {'attr': 'name'})
-        self._transport.set_resource('/foos/42/bars/1', {'prop': 'email'})
-        self._transport.set_resource('/foos/42/bazes/2', {'prop': 'email'})
-
-        payload = self._transport.get_payload('/foos/42/')
-        self.assertIsChildResource(payload, 'foo')
-
-        foo = payload['foo']
-        self.assertIn('bars', foo['links'])
-        self.assertEquals(foo['links']['bars']['href'], '/foos/42/bars/')
-        self.assertIn('bazes', foo['links'])
-        self.assertEquals(foo['links']['bazes']['href'], '/foos/42/bazes/')
-
-
 class HttpRequestTests(unittest.TestCase):
+    def setUp(self):
+        self.request = HttpRequest('/')
+
     def test_default_values(self):
-        request = HttpRequest('/')
-        self.assertEquals(request.get_url(), '/')
-        self.assertEquals(request.get_method(), 'GET')
-        self.assertEquals(request.get_body(), '')
+        self.assertEquals(self.request.get_url(), '/')
+        self.assertEquals(self.request.get_method(), 'GET')
+        self.assertIsNone(self.request.get_body())
 
     def test_custom_method(self):
+        self.request.set_method('PUT')
+        self.assertEquals(self.request.get_method(), 'PUT')
+
         request = HttpRequest('/', 'POST')
         self.assertEquals(request.get_method(), 'POST')
 
+    def test_post_form_data(self):
+        request = HttpRequest('/', 'POST')
+        request.add_field('foo', 'bar')
+        request.add_field('bar', 42)
+        request.add_field('err', 'must-be-deleted')
+        request.add_field('name', 'somestring')
+        request.del_field('err')
 
-class ResourceBuilderTests(unittest.TestCase):
-    def setUp(self):
-        transport = MockTransport()
-        transport.set_resource('/foos/1/',  {'prop': 'x'})
-        transport.set_resource('/foos/2/',  {'prop': 'y'})
-        transport.set_resource('/foos/3/',  {'prop': 'z'})
-        transport.set_resource('/bars/1/',  {'field': 'foo'})
-        transport.set_resource('/bars/2/',  {'field': 'bar'})
-        transport.set_resource('/bars/3/',  {'field': 'baz'})
-        transport.set_resource('/foos/1/bazes/1/',  {'attr': '101'})
-        transport.set_resource('/foos/2/bazes/2/',  {'attr': '102'})
-        transport.set_resource('/foos/3/bazes/3/',  {'attr': '103'})
-        transport.set_resource('/foos/3/bazes/3/quxes/1/',  {'value': 'name'})
+        ctype, content = request.get_body()
+        m = re.match('^multipart/form-data; boundary=(.*)$', ctype)
+        self.assertIsNotNone(m)
+        fields = [l.strip() for l in content.split('--' + m.group(1))][1:-1]
 
-        transport.set_resource('/reqs/1/', {'author': 'user1', 'group': 'dev'})
-        transport.set_resource('/reqs/2/', {'author': 'user2', 'group': 'gfx'})
-        transport.set_resource('/reqs/3/', {'author': 'user3', 'group': 'sfx'})
-        transport.set_resource('/reqs/1/revs/', {'file': 'main.cpp'})
-        transport.set_resource('/reqs/2/revs/', {'file': 'layout.svg'})
-        transport.set_resource('/reqs/3/revs/', {'file': 'ambient.wav'})
+        d = {}
 
-        self.transport = transport
-        self.builder = ResourceBuilder(transport)
+        for f in fields:
+            lst = f.split('\r\n\r\n')
+            self.assertEquals(len(lst), 2)
+            k, v = lst
 
-    def test_build_generic_resource(self):
-        resource = self.builder.build(Resource, {'stat': 'ok'})
-        self.assertIsInstance(resource, Resource)
+            m = re.match('Content-Disposition: form-data; name="(.*?)"$', k)
+            self.assertIsNotNone(m)
+            d[m.group(1)] = v
 
-    def test_build_custom_resource(self):
-        resource = self.builder.build(StubResource, {'stat': 'ok'})
-        self.assertIsInstance(resource, StubResource)
+        self.assertEquals(d, {'foo': 'bar', 'bar': '42', 'name': 'somestring'})
 
-    def test_build_resource_with_token(self):
-        resource = self.builder.build(Resource, {'tok': {}, 'stat': 'ok'},
-                                      token='tok')
-        self.assertIsInstance(resource, Resource)
+    def test_post_form_files(self):
+        pass
