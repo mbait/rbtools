7b7405c82619cf85be9007b154d30980cd303ade
Alexander Solovets <solovetz@vl.ru>
2012-05-25 13:52:47 +1100

    Brand new design of the client structure.
    RBClient should not depend on the transport the user will choose. It receives a
    'Transport' sub-class which takes care of how 'get_root' should actually
    behave: whether it should act asynchronously and receive a callback or
    vice-versa.

--- rbtools/api/client.py
+++ rbtools/api/client.py
@@ -1,28 +1,16 @@
 import re
 
-import rbtools.api.resource as res
+from rbtools.api.request import HttpRequst
 from rbtools.api.builder import ResourceBuilder
+from rbtools.api.transport import UrllibTransport
 
 
 class RBClient(object):
     """ Entry point for accessing RB resources through the web API.
     """
-    DEFAULT_BINDINGS = {
-        '/': res.RootResource,
-        '/review-requests/*/diffs/': res.DiffListResource,
-        '/review-requests/*/diffs/*/': res.DiffResource,
-    }
+    def __init__(self, url, transport_cls=UrllibTransport):
+            self.get_root = transport_cls(Link(ResourceBuilder(), HttpRequst(url)))
 
-    def __init__(self, url_prefix, bindings={}):
-        reg = {}
-
-        for b in [self.DEFAULT_BINDINGS, bindings]:
-            for pat, cls in b.iteritems():
-                reg[re.compile(pat.replace('*', '\w+'))] = cls
-
-        self._builder = ResourceBuilder(reg)
-
-    def get_root(self, callback=lambda: None):
-        Foo(self._builder).fetch('/', callback)
-        #callback(
-        #callback(self._builder.build('/'))
+# vim: set ts=4 :
+# vim: set sw=4 :
+# vim: set expandtab :
---
+++ rbtools/api/transport.py
@@ -0,0 +1,27 @@
+class Transport(object):
+	def __init__(self, link):
+		self._link = link
+
+
+class UrllibTransport(Transport):
+	def __int__(self, *args):
+		super(UrllibTransport, self).__init__(args)
+
+	def __call__(self, **kwargs):
+		pass
+
+
+class AsyncTransport(Transport):
+	"""Needs to be implemented."""
+	def __int__(self, *args):
+		super(AsyncTransport, self).__init__(args)
+
+	def __call__(self, callback=None, **kwargs):
+		"""Should send request asynchronously and
+		call the 'callback' upon response.
+		"""
+		pass
+
+# vim : set ts=4 :
+# vim : set sw=4 :
+# vim : set expandtab :
