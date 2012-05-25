d88c4a6e2b8f35a8282db8b6e221732ef1d166b8
Alexander Solovets <solovetz@vl.ru>
2012-05-25 16:46:32 +1100

    Make 'get_root' a property.
    Such technique increases the code readability.

--- rbtools/api/client.py
+++ rbtools/api/client.py
@@ -1,6 +1,6 @@
 import re
 
-from rbtools.api.request import HttpRequst
+from rbtools.api.request import HttpRequest
 from rbtools.api.builder import ResourceBuilder
 from rbtools.api.transport import UrllibTransport
 
@@ -9,7 +9,11 @@ class RBClient(object):
     """ Entry point for accessing RB resources through the web API.
     """
     def __init__(self, url, transport_cls=UrllibTransport):
-            self.get_root = transport_cls(Link(ResourceBuilder(), HttpRequst(url)))
+        self._root = transport_cls((ResourceBuilder(), HttpRequest(url + '/api')))
+
+    @property
+    def get_root(self):
+        return self._root
 
 # vim: set ts=4 :
 # vim: set sw=4 :
